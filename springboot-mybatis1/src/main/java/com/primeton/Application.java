792a83aa2f4ec0ee8364e9d2c6d784ebaa3c2ea6
renyh <15010633009@163.com>
2018-11-17 19:20:33 +0800

    提交springboot整合mybatis

---
+++ springboot-mybatis1/src/main/java/com/primeton/Application.java
@@ -0,0 +1,18 @@
+package com.primeton;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableSwagger2
+@ComponentScan(basePackages = { "com.primeton.spring_root_mybatis" })
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
